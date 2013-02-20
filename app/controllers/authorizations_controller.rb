0b2e1cf85d69cecf3ea06ad7dd9637849f5c1c4c
nisa balakrishnan <nisa@mobme.in>
2013-02-20 12:39:42 +0530

    authentication fixes

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -5,22 +5,29 @@ class AuthorizationsController < ApplicationController
     omniauth = request.env['omniauth.auth'] #this is where you get all the data from your provider through omniauth
     @auth = Authorization.find_from_hash(omniauth)
     registered_user = User.find_by_email(omniauth['info']['email'])
-
+     
     if @auth
       flash[:notice] = "Welcome back #{omniauth['provider']} user."
       log_in_and_redirect(@auth.user)
     else
-      user = User.new
-      user.apply_omniauth(omniauth)
-      user.reset_persistence_token
-      if user.save!
+      if registered_user
+        user = registered_user
         Authorization.create({:user_id => user.id, :provider => omniauth['provider'], :uid => omniauth['uid']}, :without_protection => true)
-        flash[:notice] = "User created and signed in successfully."
         log_in_and_redirect(user)
+        flash[:notice] = "Signed in successfully."
       else
-        session[:omniauth] = omniauth.except('extra')
-        redirect_to register_path
-      end
+        user = User.new
+        user.apply_omniauth(omniauth)
+        user.reset_persistence_token
+        if user.save!
+          Authorization.create({:user_id => user.id, :provider => omniauth['provider'], :uid => omniauth['uid']}, :without_protection => true)
+          flash[:notice] = "User created and signed in successfully."
+          log_in_and_redirect(user)
+        else
+          session[:omniauth] = omniauth.except('extra')
+          redirect_to register_path
+        end
+      end  
     end
   end
   
